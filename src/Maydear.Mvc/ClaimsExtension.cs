c51ae8a847c8ec749a26208845c18a86e4f71537
Kelvin Leung <hospp@hotmail.com>
2020-02-22 23:18:27 +0800

    获取IP地址

---
+++ src/Maydear.Mvc/ClaimsExtension.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maydear.Mvc
+{
+   public static class ClaimsExtension
+    {
+    }
+}
--- src/Maydear.Mvc/HttpContextExtension.cs
+++ src/Maydear.Mvc/HttpContextExtension.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Maydear.Mvc
 {
@@ -9,5 +12,24 @@ namespace Maydear.Mvc
     /// </summary>
     public static class HttpContextExtension
     {
+        /// <summary>
+        /// 获取用户的Ip地址。
+        /// </summary>
+        /// <param name="context">Http上下文。</param>
+        /// <returns>返回客户端ip地址</returns>
+        public static string GetUserIpAddress(this HttpContext context)
+        {
+            string ip = context.Request.Headers["X-Forwarded-For"].FirstOrDefault();
+            if (string.IsNullOrEmpty(ip))
+            {
+                ip = context.Connection.RemoteIpAddress.ToString();
+            }
+            if (ip.Any(a => a == ','))
+            {
+                return ip.Substring(0, ip.IndexOf(","));
+            }
+
+            return ip;
+        }
     }
 }
